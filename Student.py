8f8ca46c133e1c8d40b67156fbe7d50679e75480
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-18 20:42:34 +0300

    Constructor and toString

---
+++ Student.py
@@ -0,0 +1,17 @@
+
+
+class Student:
+
+    # Constructor
+    def __init__(self,ID):
+        self.ID = ID
+
+    # toString
+    def __str__(self) -> str:
+        
+        content = f"""
+        ----- Student {self.ID} -----
+        |
+        |
+        """
+        return 
